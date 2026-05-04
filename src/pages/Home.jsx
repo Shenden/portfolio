ef8f40adbb0ed38c31d9c84773c123672638fae1
shenden <adanielli@hotmail.com>
2026-05-04 12:05:34 +0200

    added video

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,36 @@
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
-
+import cosmos from "../videos/cosmos.mp4";
 export const Home = ({ title }) => {
   return (
     <>
       <motion.div
         initial={{ opacity: 0, y: 40 }}
-        animate={{ opacity: 1, y: 0 }}
+        animate={{ opacity: 1, y: 0, rotate: 0.5 }}
         className="content-home"
       >
         <motion.h1
           className="not-perfect"
-          initial={{ y: 40, rotate: 0 }}
-          animate={{ y: 0, rotate: -3 }}
+          initial={{ y: 40 }}
+          animate={{ y: 20 }}
         >
           {title}
         </motion.h1>
-
         <p>
           Love all things design & tech. Years of experience working with
           CSS/SCSS/Styled Components, some Figma and on a somewhat intermediate
           level of React.
         </p>
+        <h3> My design philosophy?</h3>
         <p>
-          My design philosophy - I believe in design being clean and clear, but
-          not too clean. To steer away from being perfect adds a playful subtle
-          human connection and evokes some warmth with the user, just like using
-          correct colors. In what ways? A fun logo, a heading that sits a bit
-          off, etc. In the end though, what matters most is a user-friendly
-          ui/ux, to engage and maintain the user on the right path, navigating a
-          site throughout articles, registration/login flows etc.
+          I believe in design being clean and clear, but not too clean. To steer
+          away from being perfect adds a playful subtle human connection and
+          evokes some warmth with the user, just like using correct colors. In
+          what ways? A fun logo, a heading that sits a bit off, etc. But most
+          important is a user-friendly ui/ux, to engage and maintain the user on
+          the right path, navigating throughout articles, registration/login
+          flows etc, with breeze.
         </p>
-
         <p>
           This site is still a wip. In the meantime, you can take a look at a
           interface concept I'm working on called &nbsp;
@@ -39,14 +38,15 @@ export const Home = ({ title }) => {
             Cosmos
           </a>
           (opens up in external window)
+          <video autoPlay muted loop playsInline>
+            <source src={cosmos} type="video/mp4" />
+          </video>
         </p>
-
         <p>
           Or &nbsp;
           <Link to="/records"> checkout my vinyl collection </Link>. A small
           one, but maybe we vibe with the same vibes!
         </p>
-
         <p>
           Ready to up my challenge in either a UI or React development position.
         </p>
