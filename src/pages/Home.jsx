d2a29aba42323c450490798fe1f01222419bf4fc
shenden <adanielli@hotmail.com>
2026-05-18 12:16:32 +0200

    txt update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,7 +35,8 @@ export const Home = ({ title }) => {
         <p>
           Love all things design & tech. Years of experience working with
           CSS/SCSS/Styled Components, Figma and beginner/intermediate level of
-          using React/Hooks/State Management/Building custom components.
+          using React/Hooks/State Management/Building custom components,
+          understanding of CI/CD flows (GitLab, GitHub, Jenkins), Jira.
         </p>
         <h3> My design philosophy?</h3>
         <p>
